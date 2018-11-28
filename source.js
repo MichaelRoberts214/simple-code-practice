f0d1a6a479bbb7449fb70791e74f0f96ca75af00
MichaelRoberts214 <marober7.mr@gmail.com>
2018-11-27 22:51:04 -0800

    Implement number to word solution

--- source.js
+++ source.js
@@ -36,4 +36,135 @@ window.onload = function () {
     
     // display accumulator
   });
+
+
+
+  console.log('the number is', numberToWord(1234516));
+};
+
+// number to words
+
+// number, no commas, no decimal yet
+
+// break number up into 1-3 digit blocks
+// turn numbers in that block into words
+// append suffix to end of word-ized block
+// put it all together
+
+// zero is an edge case
+// need a dictionary of basic numbers
+// need a dictionary of tens place numbers, excluding teens
+// need a dictionary of ten-teens numbers
+
+const splitNum = function (num) {
+  const numStr = num.toString();
+  if (numStr.length < 4) {
+    return [numStr];
+  }
+
+  const lhs = numStr.length % 3;
+  const pieces = [];
+  let currentIdx = 0;
+
+  if (lhs > 0) {
+    pieces.push(numStr.substr(0, lhs));
+    currentIdx = lhs;
+  }
+
+  while (currentIdx < numStr.length - 1) {
+    let end = currentIdx + 2;
+    pieces.push(numStr.substr(currentIdx, end));
+    currentIdx = currentIdx + 3;
+  }
+
+  return pieces;
+}
+
+const generateSuffix = function (suffixIndex) {
+  return suffixes[suffixIndex];
+};
+
+const spellOutNumber = function (numStr) {
+  const numInt = parseInt(numStr);
+  if (numStr.length === 1) {
+    return numberDict[numStr];
+  } else if (numStr.length === 2) {
+    if (numInt < 10) {
+      return spellOutNumber(numInt.toString());
+    } else if (numInt > 9 && numInt < 20) {
+      return teens[numStr];
+    } else {
+      return tensNumbers[numStr.charAt(0)] + ' ' + spellOutNumber(numStr.charAt(1));
+    }
+  } else {
+    return numberDict[numStr.charAt(0)] + ' hundred ' + spellOutNumber(numStr.substr(1, 2));
+  }
+};
+
+const numberToWord = function (num) {
+  if (num === 0) {
+    return 'zero';
+  }
+  if (num < 0) {
+    return 'negative ' + numberToWord(Math.abs(num));
+  }
+
+  const numPieces = splitNum(num);
+
+  let result = '';
+
+  for (let i = 0; i < numPieces.length; i++) {
+    result = result.length ? result + ' ' : result;
+    result += spellOutNumber(numPieces[i]);
+    const suffixIndex = numPieces.length - i;
+    if (suffixIndex > 1) {
+      result += ` ${generateSuffix(suffixIndex)}`;
+    }
+  }
+
+  return result;
+};
+
+const numberDict = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight', 
+  9: 'nine'
+};
+
+const teens = {
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'ninteen'
+};
+
+const tensNumbers = {
+  2: 'twenty',
+  3: 'thirty',
+  4: 'fourty',
+  5: 'fifty',
+  6: 'sixty',
+  7: 'seventy',
+  8: 'eighty',
+  9: 'ninty'
+};
+
+// to do: hundred, thousand, million, billion
+const suffixes = {
+  1: 'hundred',
+  2: 'thousand',
+  3: 'million',
+  4: 'billion'
 };
